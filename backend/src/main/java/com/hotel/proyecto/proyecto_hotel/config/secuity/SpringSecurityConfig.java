c1000da3a5c0fb791579d557d699267c39d73d61
AlfonsoMSDL <alfonsomanuelsalazardeleon49@gmail.com>
2026-07-20 15:52:37 -0500

    Permitir acceso público a la ruta GET /habitaciones en la configuración de seguridad

--- backend/src/main/java/com/hotel/proyecto/proyecto_hotel/config/secuity/SpringSecurityConfig.java
+++ backend/src/main/java/com/hotel/proyecto/proyecto_hotel/config/secuity/SpringSecurityConfig.java
@@ -56,6 +56,7 @@ public class SpringSecurityConfig {
                     authz
                         .requestMatchers(HttpMethod.POST,"/usuarios").permitAll()
                         .requestMatchers("/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/habitaciones").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilter(jwtAuthenticationFilter)
