1571b6c388249289178b2bac412756837bd107f7
AlfonsoMSDL <alfonsomanuelsalazardeleon49@gmail.com>
2026-07-20 15:50:42 -0500

    Modificar GetHabitacion para convertirlo en una clase y agregar el campo estadoActual

--- backend/src/main/java/com/hotel/proyecto/proyecto_hotel/dto/response/GetHabitacion.java
+++ backend/src/main/java/com/hotel/proyecto/proyecto_hotel/dto/response/GetHabitacion.java
@@ -1,11 +1,18 @@
 package com.hotel.proyecto.proyecto_hotel.dto.response;
 
-public record GetHabitacion(
-    Long id,
-    Integer numero,
-    String tipoHabitacion,
-    Double precioNoche,
-    Integer capacidad,
-    String descripcion
-) {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetHabitacion {
+    private Long id;
+    private Integer numero;
+    private String tipoHabitacion;
+    private Double precioNoche;
+    private Integer capacidad;
+    private String descripcion;
+    private String estadoActual;
 }
