c1a765316ef43f31b81fa71a957ca6e52b62922e
AlfonsoMSDL <alfonsomanuelsalazardeleon49@gmail.com>
2026-07-29 22:03:01 -0500

    Permitir acceso público a las rutas de fotos y archivos subidos en la configuración de seguridad

--- backend/src/main/java/com/hotel/proyecto/proyecto_hotel/config/secuity/SpringSecurityConfig.java
+++ backend/src/main/java/com/hotel/proyecto/proyecto_hotel/config/secuity/SpringSecurityConfig.java
@@ -56,8 +56,11 @@ public class SpringSecurityConfig {
                     authz
                         .requestMatchers(HttpMethod.POST,"/usuarios").permitAll()
                         .requestMatchers("/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html").permitAll()
+                        .requestMatchers("/uploads/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/habitaciones").permitAll()
                         .requestMatchers(HttpMethod.GET,"/habitaciones/{id}").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/fotos/habitacion/{idHabitacion}").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/fotos/first/habitacion/{idHabitacion}").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilter(jwtAuthenticationFilter)
@@ -73,6 +76,7 @@ public class SpringSecurityConfig {
                 .build();
     }
 
+    
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
